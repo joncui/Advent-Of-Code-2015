f3dca9e5e1769b9922e902f69931ba566a612cb4
Jonathan Cui <joncui1101@gmail.com>
2015-12-09 12:33:23 -0800

    Cleaning up Day4

--- Day4/day4.js
+++ Day4/day4.js
@@ -1,26 +1,26 @@
-// var rl = require('readline').createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-// });
+var rl = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 var md5 = require('md5');
+var input = "bgvyzdsv";
 
-// rl.question("Enter the secret key.\n", function(key) {
-//     console.log(findHash(key));
-//     rl.close();
-// });
+rl.question("Which part of day 4?\n", function(partNum) {
+    var startsWith = (partNum == 1 ? "00000" : "000000");
+    console.log(findHash(input, startsWith));
+    rl.close();
+});
 
-var findHash = function(key) {
+var findHash = function(key, startsWith) {
     var found = false;
     var count = -1;
     var hash;
     while (!found) {
         count++;
         hash = md5(key + count.toString());
-        if (hash.startsWith("000000")) {
+        if (hash.startsWith(startsWith)) {
             found = true;
         }
     }
     return count;
 }
-
-console.log(findHash("bgvyzdsv"));
